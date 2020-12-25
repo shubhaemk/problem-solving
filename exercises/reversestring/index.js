e90c5166c00240a6cba61d7b103be68029526dfc
Shubham Kamath <dev.shubhaem@gmail.com>
2020-12-26 03:31:27 +0530

    completed: reverse string problem with 6 solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,78 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/**
+ * Solution 1
+    function reverse(str) {
+        if (typeof str !== "string") return;
+        let reverString = "";
+
+        for (let index = str.length - 1; index >= 0; index--)
+            reverString = `${reverString}${str[index]}`;
+
+        return reverString;
+    }
+*/
+
+/**
+  * Solution 2
+    function reverse(str) {
+        return str.split("").reverse().join("");
+    }
+*/
+
+/**
+ *  Solution 3
+    function reverse(str) {
+        if (typeof str !== "string") return;
+        let reverString = "";
+
+        for (let character of str) reverString = character + reverString;
+
+        return reverString;
+    }   
+ 
+*/
+
+/**
+  * Solution 4
+    function reverse(str) {
+        return str
+            .split("")
+            .reduce((reversed, character) => character + reversed, "");
+    } 
+*/
+
+/**
+   * Solution 5 ( The Best )
+    function reverse(str, strLength) {
+        if (strLength === 0) return "";
+        const stringLength = strLength ? strLength - 1 : str.length - 1;
+        return str[stringLength] + reverse(str, stringLength);
+    }
+*/
+
+/**
+   * Solution 6  
+    function reverse(str) {
+        const strLength = str.length;
+        if (!strLength) return "";
+
+        const lastLetter = str[strLength - 1];
+        str = str.substring(0, str.length - 1);
+
+        return lastLetter + reverse(str);
+    }
+*/
+
+function reverse(str) {
+  const strLength = str.length;
+  if (!strLength) return "";
+
+  const lastLetter = str[strLength - 1];
+  str = str.substring(0, str.length - 1);
+
+  return lastLetter + reverse(str);
+}
 
 module.exports = reverse;
