e452c380e191ddb278035b6b2b976fcef0475c4a
Shubham Kamath <dev.shubhaem@gmail.com>
2020-12-28 00:34:15 +0530

    completed: max character

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let maxChar = null;
+  let maxCharObject = {};
+  for (let char of str) {
+    if (maxCharObject[char]) {
+      maxCharObject[char] += 1;
+    } else {
+      maxCharObject[char] = 1;
+    }
+
+    if (maxChar) {
+      if (maxCharObject[char] > maxChar.count) {
+        maxChar["character"] = char;
+        maxChar["count"] = maxCharObject[char];
+      }
+    } else {
+      maxChar = {};
+      maxChar["character"] = char;
+      maxChar["count"] = maxCharObject[char];
+    }
+  }
+
+  return maxChar["character"];
+}
 
 module.exports = maxChar;
