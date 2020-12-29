1ed829e59d4be9c73b907c33c5a19528dc5a62e3
Shubham Kamath <dev.shubhaem@gmail.com>
2020-12-30 01:26:14 +0530

    completed: array chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  if (size < 0) {
+    size = size * -1;
+  }
+  if (array.length <= size) return [[...array]];
+
+  const chunkArray = [];
+  let tempArray = [];
+
+  for (const item of array) {
+    tempArray.length !== size && tempArray.push(item);
+    if (tempArray.length === size) {
+      chunkArray.push(tempArray);
+      tempArray = [];
+    }
+  }
+
+  if (tempArray.length) {
+    chunkArray.push(tempArray);
+  }
+
+  return chunkArray;
+}
 
 module.exports = chunk;
