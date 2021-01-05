020fe3e107c15b34b39c763e151239b02932a858
Shubham Kamath <dev.shubhaem@gmail.com>
2021-01-06 00:59:01 +0530

    completed: anagram exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,68 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/**
+ * Solution 1 
+    function anagrams(stringA, stringB) {
+        const getSortedArray = (str) =>
+            str
+            .replace(/[^\w]/g, "")
+            .split("")
+            .map((letter) => letter.toLowerCase())
+            .sort();
+
+        const stringAArray = getSortedArray(stringA);
+        const stringBArray = getSortedArray(stringB);
+
+        if (stringAArray.length !== stringBArray.length) return false;
+
+        for (let index = 0; index < stringAArray.length; index++) {
+            if (stringAArray[index] !== stringBArray[index]) {
+            return false;
+            }
+        }
+
+        return true;
+        }
+ */
+
+function anagrams(stringA, stringB) {
+  const stringAFiltered = stringA.replace(/[^\w]/g, "");
+  const stringBFiltered = stringB.replace(/[^\w]/g, "");
+
+  if (stringAFiltered.length !== stringBFiltered.length) {
+    return false;
+  }
+
+  const generateCharacterMap = (str) => {
+    const characterMap = {};
+
+    for (let character of str) {
+      if (characterMap[character]) {
+        characterMap[character] += 1;
+      } else {
+        characterMap[character] = 1;
+      }
+    }
+
+    return characterMap;
+  };
+
+  const stringACharacterMap = generateCharacterMap(stringAFiltered);
+  const stringBCharacterMap = generateCharacterMap(stringBFiltered);
+
+  if (
+    Object.keys(stringACharacterMap).length !==
+    Object.keys(stringBCharacterMap).length
+  ) {
+    return false;
+  }
+
+  return Object.keys(stringACharacterMap).every(
+    (key) => stringACharacterMap[key] === stringBCharacterMap[key]
+  );
+}
+
+console.log(anagrams("hello", "lloeh"));
 
 module.exports = anagrams;
