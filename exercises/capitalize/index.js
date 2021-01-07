95616dfafd8e2ab7eab8984a17053732ba286d60
Shubham Kamath <dev.shubhaem@gmail.com>
2021-01-08 02:41:10 +0530

    completed: capitalize with 4 solutions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,60 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+/**
+ *  Solution 1
+    function capitalize(str) {
+    return str
+        .split(" ")
+        .map((word) => `${word[0].toUpperCase()}${word.substr(1, word.length)}`)
+        .join(" ");
+    }
+ */
+
+/**
+  * Solution 2
+  function capitalize(str) {
+    let isSpace = true;
+    let capitalizedString = "";
+
+    for (let characterIndex = 0; characterIndex < str.length; characterIndex++) {
+        if (str[characterIndex].trim() === "") {
+        isSpace = true;
+        capitalizedString = `${capitalizedString} `;
+        continue;
+        }
+        if (isSpace) {
+        capitalizedString = `${capitalizedString}${str[
+            characterIndex
+        ].toUpperCase()}`;
+        isSpace = false;
+        continue;
+        }
+
+        capitalizedString = `${capitalizedString}${str[characterIndex]}`;
+    }
+
+    return capitalizedString;
+    } 
+  */
+
+/**
+   *  Solution 3
+   function capitalize(str) {
+    return str
+        .split(" ")
+        .map((word) => `${word[0].toUpperCase()}${word.slice(1)}`)
+        .join(" ");
+    }
+   */
+
+function capitalize(str) {
+  let capitalizedString = "";
+  for (let characterIndex = 0; characterIndex < str.length; characterIndex++)
+    characterIndex === 0 || str[characterIndex - 1].trim() === ""
+      ? (capitalizedString += str[characterIndex].toUpperCase())
+      : (capitalizedString += str[characterIndex]);
+  return capitalizedString;
+}
 
 module.exports = capitalize;
