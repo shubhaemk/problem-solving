984c55a6621fd212baff1a9d4b2c0f6066de42ef
Shubham Kamath <dev.shubhaem@gmail.com>
2021-01-09 03:30:28 +0530

    completed: steps with 6 solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -37,26 +37,85 @@ function steps(n) {
 }  
 */
 
-function steps(n) {
-  const getRepeatativeString = (str, count) => {
-    let finalString = "";
-    while (count > 0) {
-      finalString += str;
-      count--;
-    }
-    return finalString;
-  };
+/**
+ * Solution 3
+ function steps(n) {
+    const getRepeatativeString = (str, count) => {
+      let finalString = "";
+      while (count > 0) {
+        finalString += str;
+        count--;
+      }
+      return finalString;
+    };
 
-  let index = 1;
-  while (index <= n) {
-    console.log(
-      `${getRepeatativeString("#", index)}${getRepeatativeString(
-        " ",
-        n - index
-      )}`
-    );
-    index++;
+    let index = 1;
+    while (index <= n) {
+      console.log(
+        `${getRepeatativeString("#", index)}${getRepeatativeString(
+          " ",
+          n - index
+        )}`
+      );
+      index++;
+    }
   }
+ */
+
+/**
+  * Solution 4
+  function steps(n) {
+    for (let row = 0; row < n; row++) {
+      let stair = "";
+
+      for (let column = 0; column < n; column++) {
+        if (column <= row) {
+          stair += "#";
+        } else {
+          stair += " ";
+        }
+      }
+
+      console.log(stair);
+    }
+  }
+  */
+
+/**
+  * Solution 5
+  function steps(n, step = 1) {
+    const emptySteps = n - step;
+
+    let finalStep = "";
+    for (let index = 0; index < step; index++) finalStep += "#";
+    for (let index = 0; index < emptySteps; index++) finalStep += " ";
+
+    console.log(finalStep);
+
+    if (emptySteps === 0) {
+      return;
+    } else {
+      steps(n, step + 1);
+    }
+  }
+  */
+// solution 5 is the best
+function steps(n, step = 0, stair = "") {
+  if (n === step) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, step + 1);
+  }
+
+  if (stair.length <= step) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, step, stair);
 }
 
 module.exports = steps;
