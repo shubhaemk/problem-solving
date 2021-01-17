743f36466cd349193e6097b9b47e3893159d2afb
Shubham Kamath <dev.shubhaem@gmail.com>
2021-01-18 04:40:32 +0530

    completed: queue ds

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,62 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+/**
+ * add(item) -> push()
+ * []
+ * [1]
+ * [1,2]
+ * [1,2,3]
+ * ------>
+ * remove() -> filter()
+ * [1,2,3]
+ * [2,3]
+ * [3]
+ * <-----
+ */
+class QueueInvert {
+  constructor() {
+    this.data = [];
+  }
+
+  add(item) {
+    this.data = [...this.data, item];
+  }
+
+  remove() {
+    let removedItem = this.data[0];
+    this.data = this.data.filter((item, index) => index !== 0);
+
+    return removedItem;
+  }
+}
+
+/**
+ * add(item) -> unshift()
+ * []
+ * [1]
+ * [2,1]
+ * [3,2,1]
+ * <------
+ * remove() -> pop()
+ * [3,2,1]
+ * [3,2]
+ * [3]
+ * ----->
+ */
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(item) {
+    this.data.unshift(item);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
